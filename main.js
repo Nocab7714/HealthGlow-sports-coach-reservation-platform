f85f3051cb96fa0282d881592e9a5f201c18da14
peihanWang <nocab7714@gmail.com>
2024-09-26 22:52:41 +0800

    feat : 環境設置
    1. 安裝相關套件
    2. 設置 scss 路徑
    3. 新增圖片資源

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
+
+// swiper
+import './swiper';
+
+// AOS
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// AOS init
+AOS.init();
---
+++ swiper.js
@@ -0,0 +1,28 @@
+// Swiper
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+// init Swiper
+// const swiperHighlightedArticles = new Swiper('.swiperHighlightedArticles', {
+//   slidesPerView: 1.1,
+//   spaceBetween: 24,
+//   slidesPerGroup: 1,
+//   autoHeight: true,
+
+//   navigation: {
+//     nextEl: '.swiper-next',
+//     prevEl: '.swiper-prev',
+//   },
+
+//   pagination: {
+//     el: '.swiper-pagination-HighlightedArticles',
+//     type: 'fraction',
+//   },
+
+//   breakpoints: {
+//     768: {
+//       slidesPerView: 2,
+//       slidesPerGroup: 2,
+//       spaceBetween: 48,
+//     },
+//   },
+// });
