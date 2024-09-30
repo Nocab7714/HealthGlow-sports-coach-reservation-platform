a8ad2adf4ec356a635419bbfa4a28cf3706995c6
peihanWang <nocab7714@gmail.com>
2024-09-30 20:30:03 +0800

    feat : 調整 bootstrap icon 引入

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
+// bootstrap-icons
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 // swiper
 import './swiper';
 
