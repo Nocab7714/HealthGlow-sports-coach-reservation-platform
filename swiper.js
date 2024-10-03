81ac81d48d76fb481a9faf91ca766434c63c516d
sonia <soniayjw@gmail.com>
2024-10-03 22:18:31 +0800

    add：新增首頁navbar的三個dropdown menu(含flatpicker)、新增首頁推薦課程區塊(包含swiper)、新增首頁痛點區塊(含AOS)
    1. 載入 material symbols outlined、material symbols rounded、flatpicker
    2. 共新增 6 個客製 components (list-group、nav-tabs、card、dropdown、buttons、flatpicker)
    3. 新增 scss > vendor > swiper.scss
    4. 新增變數 (font-size、gray 顏色)
    5. 修改元件變數 (nav-tabs-link-active-color、line-height)
    6. 啟用負值 margin
    7. 調整 utilities (啟用部分 hover 及 RWD、新增 width 選項、新增 overflow 選項 )

--- swiper.js
+++ swiper.js
@@ -26,3 +26,31 @@ import 'swiper/css/bundle';
 //     },
 //   },
 // });
+
+// Navigation 首頁課程推薦區塊
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1.5,
+  centeredSlides: true,
+  spaceBetween: 20,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints:{
+    768: {
+      slidesPerView: 2,
+      centeredSlides: false,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 24,
+      centeredSlides: false,
+    },
+
+  }
+
+});
