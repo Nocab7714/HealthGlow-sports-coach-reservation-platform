d61a82edf8631f4d360d0e5893209c2c36a46ad6
peihanWang <nocab7714@gmail.com>
2024-10-16 22:03:51 +0800

    feat : 調整插件引入方式、更多內容顯示功能製作 ( 未完成 )

--- main.js
+++ main.js
@@ -15,15 +15,3 @@ AOS.init();
 
 // GSAP
 import './gsap.js';
-
-// jQuery
-import $ from 'jquery';
-
-// lightbox2
-import 'lightbox2/dist/css/lightbox.css';
-import 'lightbox2';
-lightbox.option({
-  fitImagesInViewport: true, // 確保圖片在視窗中縮放以適應大小
-  imageFadeDuration: 300, // 圖片加載後淡入的時間（毫秒）。
-  fadeDuration: 300, // 圖片大小變換時的動畫時長
-});
