061d27458454a754cca1d75e14d7c364e86d8584
peihanWang <nocab7714@gmail.com>
2024-11-01 13:29:07 +0800

    feat : 修改 lightbox2 套件引入方式

--- main.js
+++ main.js
@@ -20,11 +20,4 @@ import './plugins/flatpickr.js';
 import './plugins/gsap.js';
 
 // lightbox2
-import 'lightbox2/dist/css/lightbox.min.css';
-import lightbox from 'lightbox2';
-// lightbox2
-lightbox.option({
-  fitImagesInViewport: true, // 確保圖片在視窗中縮放以適應大小
-  imageFadeDuration: 300, // 圖片加載後淡入的時間（毫秒）。
-  fadeDuration: 300, // 圖片大小變換時的動畫時長
-});
+import './plugins/lightbox2.js';
